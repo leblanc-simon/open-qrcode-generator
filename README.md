d64411afb8cc2e961ef9d07b4e24bb201150d30b
Simon Leblanc <contact@leblanc-simon.eu>
2021-12-19 19:18:24 +0100

    License and readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Open QRCode Generator
+
+## Installation
+
+```bash
+git clone https://github.com/leblanc-simon/open-qrcode-generator.git
+cd open-qrcode-generator
+composer install
+```
+
+Your `DOCUMENT_ROOT` must be `[install_directory]/public`.
+
+## License
+
+* [WTFPL](http://www.wtfpl.net/txt/copying/)
--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Copyright © 2021 Simon Leblanc <contact@leblanc-simon.eu>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
 require_once __DIR__.'/../vendor/autoload.php';
 
 if (false === isset($_POST['content']) || false === is_string($_POST['content']) || true === empty($_POST['content'])) {
